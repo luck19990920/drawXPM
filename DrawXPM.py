ba51e8e03aa61c86bd70db40ddc4287497698f75
luck19990920 <897320551@qq.com>
2024-07-08 14:23:19 +0800

    first commit

---
+++ DrawXPM.py
@@ -0,0 +1,171 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os.path
+from matplotlib import _rc_params_in_file # type: ignore
+from pathlib import Path
+mplstyle = ['./style/no-latex.mplstyle',
+            './style/my.mplstyle']
+
+print(f'Draw filled contours from xpm file by gmx densmap.\n'
+      f'Version 1.0, release date: 2024-Jun-25\n'
+      f'Programmed by Jian Zhang (jian_zhang@cug.edu.cn)\n'
+)
+class XPM:
+    def __init__(self, filename):
+        self.filename = filename
+        self.parse()
+
+    
+    def parse(self):
+        with open(self.filename, encoding='utf-8') as xpm:
+            while True:
+                line = xpm.readline()
+                
+                # 提取legend
+                if self.uncomment(line).strip().startswith('legend:'):
+                    self.legend = self.uncomment(line).strip().split(':')[-1]
+
+                # 提取x-label
+                if self.uncomment(line).strip().startswith('x-label:'):
+                    self.x_label = self.uncomment(line).strip().split(':')[-1]
+
+                # 提取y-label
+                if self.uncomment(line).strip().startswith('y-label:'):
+                    self.y_label = self.uncomment(line).strip().split(':')[-1]
+
+                if line.strip().startswith('static char *gromacs_xpm[]'):
+                    break
+            dim = xpm.readline()
+            nx, ny, nc, nb = [int(i) for i in dim.strip().split('"')[1].split()]
+            color = dict()
+            for _ in range(nc):
+                ls = xpm.readline().strip().split('"')
+                color[ls[1][0]] = eval(ls[3])
+            # 下面开始读取坐标轴数据
+            data = np.zeros((int(nx / nb), ny))
+            """
+            每一列中为颜色的十六进制,即把<Pixels>字段换成颜色的十六进制
+            """
+            xval = []
+            yval = []
+            line = xpm.readline()
+            while not line.strip().startswith('"'):
+                s = self.uncomment(line).strip()
+                if s.startswith('x-axis:'):
+                    xval.extend([float(x) for x in s.strip('x-axis:  ').split()])
+                elif s.startswith('y-axis:'):
+                    yval.extend([float(x) for x in s.strip('y-axis:  ').split()])
+                line = xpm.readline()
+            for iy in range(ny):
+                data[:, iy] = [color[self.unquote(line)[k:k+nb]] for k in range(0, nx, nb)]
+                line = xpm.readline()
+            # 把data反向
+            self.array = data[:,::-1]
+            if len(xval) > nx:
+                xval.pop()
+                yval.pop()
+            self.xvalues = np.array(xval)
+            self.yvalues = np.array(yval)
+
+
+    @staticmethod
+    def uncomment(s):
+        # 去除/*与*/
+        return s[2+s.find("/*") : s.rfind('*/')]
+    
+
+    @staticmethod
+    def unquote(s):
+        # 去除左右两侧的"
+        return s[1+s.find('"') : s.rfind('"')]
+
+file_path = None
+while True:
+    file_path = input('Please enter the path to your xpm file\n')
+    if os.path.isfile(file_path):
+        break
+
+XPM_object = XPM(file_path)
+x_label_str = XPM_object.x_label.split('"')[1]
+y_label_str = XPM_object.y_label.split('"')[1]
+legend_str = XPM_object.legend
+cmap = 'jet'
+alpha = 1
+min_value = np.min(XPM_object.array)
+max_value = np.max(XPM_object.array)
+num_color_colorbar = 100
+coor_scle_bool = False
+dpi = 300
+label_colorbar = 'Density'
+color_scle_bool = False
+
+while True:
+    print(f'0  x_label: {x_label_str}')
+    print(f'1  y_label: {y_label_str}')
+    print(f'2  style of colorbar: {cmap}')
+    print(f'3  alpha: {alpha}')
+    print(f'4  the number of color in colorbar: {str(num_color_colorbar)}')
+    print(f'5  Whether to turn on coordinate scale: {str(coor_scle_bool)}')
+    print(f'6  dpi: {str(dpi)}')
+    print(f'7  style file: {', '.join(mplstyle)}')
+    print(f'8  the label of colorbar: {label_colorbar}')
+    print(f'9  Whether to display the color bar scale: {color_scle_bool}')
+    print(f'd  start to draw')
+    print(f'the range of values: ({min_value}, {max_value})')
+    
+    match input():
+        case '0':
+            x_label_str = input('Please enter label of x axis.\n')
+        case '1':
+            y_label_str = input('Please enter label of y axis.\n')
+        case '2':
+            cmap = input('Please enter style of colorbar. Refer to: https://matplotlib.org/stable/users/explain/colors/colormaps.html\n')
+        case '3':
+            alpha = eval(input('Please enter alpha(0,1).\n'))
+        case '4':
+            num_color_colorbar = eval(input('Please enter the number of color in colorbar.\n'))
+        case '5':
+            coor_scle_bool = not coor_scle_bool
+        case '6':
+            dpi = int(input('Please enter dpi.\n'))
+        case '7':
+            mplstyle.append(input('Please the path of style file.\n'))
+        case '8':
+            label_colorbar = input('Please enter label of colorbar.\n')
+        case '9':
+            color_scle_bool = not color_scle_bool
+        case 'd':
+            break
+        case _:
+            print('Invalid value. Use default value.')
+
+print('Analyzing data...')
+
+styles = {}
+for path in mplstyle:
+    path = Path(path)
+    if path.is_file():
+        styles[path.stem] = _rc_params_in_file(path)
+        plt.style.core.update_nested_dict(plt.style.library, styles) # type: ignore
+        plt.style.core.available[:] = sorted(plt.style.library.keys())
+plt.style.use(list(styles.keys()))
+
+X, Y = np.meshgrid(XPM_object.xvalues, XPM_object.yvalues)
+Z = XPM_object.array
+h = plt.contourf(X, Y, Z.transpose(), np.linspace(min_value, max_value, num_color_colorbar),cmap=cmap, alpha=alpha)
+if coor_scle_bool:
+    h.axes.set_xlabel(x_label_str)
+    h.axes.set_ylabel(y_label_str)
+else:
+    h.axes.axis('off')
+cbar = plt.colorbar(h)
+# cbar.ax.set_xticks([])
+if not color_scle_bool:
+    cbar.ax.set_yticks([min_value, max_value], labels=['Low', 'High'])
+    cbar.ax.tick_params(right=False)
+cbar.set_label(label_colorbar)
+plt.savefig('./XPM_draw.png', dpi=dpi)
+plt.show()
+
+
+
---
+++ README.md
@@ -0,0 +1,77 @@
+`drawXPM`是用于绘制由GROMACS中`densmap`命令得到的`xpm`文件的工具。相较于其他的同类型`xpm`文件绘图工具，`drawXPM`有着灵活、便捷的特点，无需绘图者有任何编程基础，点开`drawXPM`的可执行文件即可作图。
+
+### 使用方法
+#### 基本操作
+目录下的`DrawXPM.py`为`DrawXPM`软件的python源代码。`drawXPM.exe`为其的可执行文件。双击`drawXPM.exe`就可以运行该软件。运行该软件后，首先会看到以下的内容：
+```
+Draw filled contours from xpm file by gmx densmap.
+Version 1.0, release date: 2024-Jun-25
+Programmed by Jian Zhang (jian_zhang@cug.edu.cn)
+
+Please enter the path to your xpm file
+```
+随后，直接将需要绘图的`xpm`文件拉进软件，然后会出现以下的内容：
+```
+0  x_label: x (nm)
+1  y_label: z (nm)
+2  style of colorbar: jet
+3  alpha: 1
+4  the number of color in colorbar: 100
+5  Whether to turn on coordinate scale: False
+6  dpi: 300
+7  style file: ./style/no-latex.mplstyle, ./style/my.mplstyle
+8  the label of colorbar: Density
+9  Whether to display the color bar scale: False
+d  start to draw
+the range of values: (0.0, 83.9)
+```
+* 按`0`：修改图像的`x`轴坐标名称，默认值从`xpm`文件中读取
+* 按`1`：修改图像的`y`轴坐标名称，默认值从`xpm`文件中读取
+* 按`2`：修改色彩条样式，默认为`jet`样式
+* 按`3`：修改图像的透明度(值域为0至1)，默认为`1`
+* 按`4`：修改色彩条上颜色的个数，该值越大颜色的色彩变化越平滑，默认为`100`
+* 按`5`：是否显示`x`轴和`y`轴坐标名称，默认为不显示
+* 按`6`：生成图像的分辨率，默认为`300`
+* 按`7`：设置作图格式文件路径，默认为`./style/no-latex.mplstyle`和`./style/my.mplstyle`
+* 按`8`：设置色彩条标签，默认为`Density`
+* 按`9`：是否显示色彩条刻度，默认不显示
+* 按`d`：开始作图
+此外，最后一行`the range of values: (0.0, 83.9)`显示色彩条的取值范围。
+
+#### 色彩条样式的修改
+色彩条样式可支持以下的样式。
+<div style="margin: auto"><img src="./example/colormap_1.png" alt='此图无法显示' style='zoom:80%;'/></div>
+<div style="margin: auto"><img src="./example/colormap_2.png" alt='此图无法显示' style='zoom:80%;'/></div>
+<div style="margin: auto"><img src="./example/colormap_3.png" alt='此图无法显示' style='zoom:80%;'/></div>
+关于色彩条样式的详细情况，请访问<a href="https://matplotlib.org/stable/users/explain/colors/colormaps.html" target="_blank">Choosing Colormaps in Matplotlib</a>
+
+### 绘图效果
+* 采用gmx自带的xpm2ps命令作图
+  <div style="text-align:center;">
+    <img src="./example/densmap_xpm2ps.png" alt="图片无法显示" height="500px" width="500px" />
+  </div>
+* 采用<a href="http://bbs.keinsci.com/thread-20056-1-1.html" target="_blank">plot_xpm</a>作图
+  <div style="text-align:center;">
+    <img src="./example/densmap_plot_xpm.png" alt="图片无法显示" height="400px" width="600px" />
+  </div>
+* 采用DrawXPM作图
+  <div style="text-align:center;">
+    <img src="./example/densmap_drawXPM.png" alt="图片无法显示" height="400px" width="600px" />
+  </div>
+
+### 鸣谢
+在开发`DrawXPM`的过程中，主要使用到了以下的Python开源模组
+* NumPy, https://numpy.org/
+* Matplotlib, https://matplotlib.org/
+* PyInstaller, https://pyinstaller.org/en/stable/
+
+此外，还参考了以下Python脚本或开源模组的处理方式
+* GromacsWrapper, https://gromacswrapper.readthedocs.io/en/latest/
+* SciencePlots, https://github.com/garrettj403/SciencePlots
+* plot_xpm, http://bbs.keinsci.com/thread-20056-1-1.html
+
+在此，对上述模组或脚本的开发者表示感谢。
+
+### 许可证
+`DrawXPM`基于MIT许可证开源。这意味着您可以自由地使用，修改和分发代码。
+
